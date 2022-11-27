405e1ae7a641ee9bca65ba4fbd6eb93d9818419e
Voyage <voyage@miouyouyou.fr>
2022-11-27 06:09:17 +0100

    Added some IK LookAt Script
    They're still very rough, but it does the trick.
    Just handle the weight variables through animations
    and you should be done.
    
    Signed-off-by: Voyage <voyage@miouyouyou.fr>

---
+++ Udon/AreaSnitch.cs
@@ -0,0 +1,46 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AreaSnitch : UdonSharpBehaviour
+{
+    public UdonBehaviour[] reportTo;
+    public string behavioursPlayerVariable = "snitchedTarget";
+    public string behavioursLocalPlayerVariable = "snitchedLocalTarget";
+
+    private void Report(string playerVariableName, VRCPlayerApi player)
+    {
+        int nReports = reportTo.Length;
+        for (int i = 0; i < nReports; i++)
+        {
+            UdonBehaviour behaviour = reportTo[i];
+            if (behaviour == null)
+            {
+                continue;
+            }
+            behaviour.SetProgramVariable(playerVariableName, player);
+        }
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        Report(behavioursPlayerVariable, player);
+        if (player.isLocal)
+        {
+            Report(behavioursLocalPlayerVariable, player);
+        }
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        Report(behavioursPlayerVariable, null);
+        if (player.isLocal)
+        {
+            Report(behavioursLocalPlayerVariable, null);
+        }
+    }
+
+    
+}
---
+++ Udon/IKLookAt.cs
@@ -0,0 +1,42 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class IKLookAt : UdonSharpBehaviour
+{
+    Animator animator;
+    public bool active = false;
+    public Transform target;
+    public float lookAtWeight = 1;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (animator == null)
+        {
+            Debug.LogError($"{gameObject.name} IKLookAt : No Animator set on the object !");
+            Debug.LogError("Engaging Self Destruct Sequence !");
+            Destroy(this);
+        }
+    }
+
+    public bool EverythingOk()
+    {
+        return ((animator != null) & (active == true) & (target != null));
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (!EverythingOk())
+        {
+            animator.SetLookAtWeight(0);
+            return;
+        }
+
+        animator.SetLookAtWeight(lookAtWeight);
+        animator.SetLookAtPosition(target.position);
+    }
+}
